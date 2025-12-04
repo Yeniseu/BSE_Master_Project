af998903bf43e11ec14d61b04f68a78fcf663246
Ece <ecetasanece@gmail.com>
2025-12-04 02:19:39 +0100

    graphs

--- 01_RScript/11_Random_Forest.R
+++ 01_RScript/11_Random_Forest.R
@@ -55,8 +55,65 @@ rf.rolling.window=function(Y,nprev,indice=1,lag=1){
   }
   
   real=Y[,indice]
-  plot(real,type="l")
-  lines(c(rep(NA,length(real)-nprev),save.pred),col="red")
+  
+  #Plots
+ 
+  plot_dir <- "03_Output/Graphs/plots_rf"
+  
+  if (!dir.exists("03_Output"))          dir.create("03_Output")
+  if (!dir.exists("03_Output/Graphs"))   dir.create("03_Output/Graphs")
+  if (!dir.exists(plot_dir))             dir.create(plot_dir)
+  
+  pred_full <- c(rep(NA, length(real) - nprev), save.pred)
+  
+  plot_file_full <- file.path(plot_dir, sprintf("rf_lag%d_full.png", lag))
+  
+  png(plot_file_full,
+      width  = 1920,
+      height = 1080,
+      res    = 300,
+      type   = "cairo-png")
+  
+  plot(real, type = "l",
+       main = sprintf("Random Forest Forecast – Lag %d (Full Series)", lag),
+       xlab = "Time",
+       ylab = "Inflation")
+  lines(pred_full, col = "red", lwd = 2)
+  legend("topleft",
+         legend = c("Actual", "RF Forecast"),
+         col    = c("black", "red"),
+         lty    = 1,
+         bty    = "n")
+  
+  dev.off()
+  
+  #Plot 2: Shorter period
+  start_idx <- max(1, length(real) - 2 * nprev + 1)
+  
+  real_zoom <- real[start_idx:length(real)]
+  pred_zoom <- pred_full[start_idx:length(real)]
+  
+  plot_file_zoom <- file.path(plot_dir, sprintf("rf_lag%d_last2nprev.png", lag))
+  
+  png(plot_file_zoom,
+      width  = 1920,
+      height = 1080,
+      res    = 300,
+      type   = "cairo-png")
+  
+  plot(real_zoom, type = "l",
+       main = sprintf("Random Forest Forecast – Lag %d (Last %d Periods)", lag, 2 * nprev),
+       xlab = "Time",
+       ylab = "Inflation")
+  lines(pred_zoom, col = "red", lwd = 2)
+  legend("topleft",
+         legend = c("Actual", "RF Forecast"),
+         col    = c("black", "red"),
+         lty    = 1,
+         bty    = "n")
+  
+  dev.off()
+
   
   rmse=sqrt(mean((tail(real,nprev)-save.pred)^2))
   mae=mean(abs(tail(real,nprev)-save.pred))
@@ -71,18 +128,25 @@ rf.rolling.window=function(Y,nprev,indice=1,lag=1){
 # First Out of Sample Predictions: 
 # Out of Sample Period : 2001-2015
 
-Y <- fred[date < "2001-01-01"]
+Y <- fred[date < "2016-01-01"]
 Y <- Y[, date := NULL]
 Y <- as.matrix(Y)
 dim(Y)
 
 # Out of Sample Length = 132
-nprev <- 132
+nprev <- 
 
 set.seed(123)
-rf1 <- rf.rolling.window(Y,nprev,1,1)
+rf1_1 <- rf.rolling.window(Y,nprev,1,1)
+
+rf3_1 <- rf.rolling.window(Y,nprev,1,3)
+
+rf <- rf.rolling.window(Y,nprev,1,6)
 
 rf1$errors
 rf1$pred
 
 str(rf1)
+
+
+2015- 2001 +1
